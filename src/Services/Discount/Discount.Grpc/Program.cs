00c7ba6fa84dad5c088f5f63acbf5527980cc38d
fungcww <62454131+fungcww@users.noreply.github.com>
2025-06-05 02:25:05 +0800

    0605

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
